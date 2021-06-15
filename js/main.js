76cab0e2c070d19ad3f564c98c0ba095891d6479
Artem_Krol <artemkrol47@gmail.com>
2021-06-15 23:04:55 +0300

    add ref to js file

--- js/main.js
+++ js/main.js
@@ -7,18 +7,12 @@ document.addEventListener('DOMContentLoaded', function () {
     e.preventDefault();
     let formData = new FormData(form);
 
-
-    console.log(form);
-    console.log(formData);
-
-    let response = await fetch('../sendmail2.php', {
+    let response = await fetch("../sendmail2.php", {
       method: 'POST',
-      body: formData,
-
+      body: formData
     });
     if (response.ok) {
       let result = await response.json();
-      console.log(result);
       form.reset();
     } else alert('error');
   }
