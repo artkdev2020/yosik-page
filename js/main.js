b8e689c50855e67afefa9cdc18783da6b5cfba4f
Kate Iskra <kateiskra99@gmail.com>
2021-06-15 14:31:57 +0300

    page project2 refactoring

--- js/main.js
+++ js/main.js
@@ -1,37 +1,19 @@
-$(function(){
-    $('.slider').slick({
-        prevArrow:'<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt=" prev arrow"></button>',
-        nextArrow:'<button class="slick-arrow slick-next"><img src="images/next.svg" alt="next arrow"></button>',
-        fade: true,
-        responsive: [
-            {
-                breakpoint: 441,
-                settings: {
-                    arrows: false
-                }
-            }
-        ]
-    });
-});
-
 document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('form');
-    //console.log(form);
-    form.addEventListener('submit', formSend);
+  const form = document.getElementById('form');
+  //console.log(form);
+  form.addEventListener('submit', formSend);
 
-    async function formSend(e) {
-      e.preventDefault();
-      let formData = new FormData(form);
+  async function formSend(e) {
+    e.preventDefault();
+    let formData = new FormData(form);
 
-      let response = await fetch("../sendmail2.php", {
-        method: "POST",
-        body:formData
-      });
-      if(response.ok) {
+    let response = await fetch('../sendmail2.php', {
+      method: 'POST',
+      body: formData,
+    });
+    if (response.ok) {
       let result = await response.json();
       form.reset();
-      }
-      else
-        alert('error');
-    }
-  })
+    } else alert('error');
+  }
+});
