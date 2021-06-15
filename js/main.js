debb7efebffed95ea988e020be705658de30fb69
Artem_Krol <artemkrol47@gmail.com>
2021-06-15 22:19:47 +0300

    home page refactoring

--- js/main.js
+++ js/main.js
@@ -7,12 +7,18 @@ document.addEventListener('DOMContentLoaded', function () {
     e.preventDefault();
     let formData = new FormData(form);
 
+
+    console.log(form);
+    console.log(formData);
+
     let response = await fetch('../sendmail2.php', {
       method: 'POST',
       body: formData,
+
     });
     if (response.ok) {
       let result = await response.json();
+      console.log(result);
       form.reset();
     } else alert('error');
   }
