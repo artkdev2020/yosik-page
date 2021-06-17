792a263c7a972694f212e3219abf89bc841a705d
Kate Iskra <kateiskra99@gmail.com>
2021-06-17 10:58:21 +0300

    modal beggining

--- js/form.js
+++ js/form.js
@@ -13,6 +13,23 @@ document.addEventListener('DOMContentLoaded', function () {
     if (response.ok) {
       let result = await response.json();
       form.reset();
+
+      let modal = document.getElementById('myModal');
+      let span = document.getElementsByClassName('close')[0];
+
+      // btn.onclick = function () {
+      modal.style.display = 'block';
+      // };
+
+      span.onclick = function () {
+        modal.style.display = 'none';
+      };
+
+      window.onclick = function (event) {
+        if (event.target == modal) {
+          modal.style.display = 'none';
+        }
+      };
     } else alert('error');
   }
 });
